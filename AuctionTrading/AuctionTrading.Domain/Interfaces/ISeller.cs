30160dbea1bc9f1503b0db9e06b8283a3bc1473c
Ant <Ant@WIN-5HKCKH8PJ6J>
2024-07-16 15:03:25 +0700

    Refactoring

--- AuctionTrading/AuctionTrading.Domain/Interfaces/ISeller.cs
+++ AuctionTrading/AuctionTrading.Domain/Interfaces/ISeller.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AuctionTrading.Domain.Entities;
 
-namespace AuctionTrading.Domain.Entities
+namespace AuctionTrading.Domain.Interfaces
 {
     internal interface ISeller : IVisitor
     {
