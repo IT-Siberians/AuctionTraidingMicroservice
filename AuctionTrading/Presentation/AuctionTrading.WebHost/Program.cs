fd91429aad7e40d3e4934660433dac11ebd347e4
Yana-Kryukova <krjanik@icloud.com>
2024-10-13 17:09:37 +0700

    #GD-106

--- AuctionTrading/Presentation/AuctionTrading.WebHost/Program.cs
+++ AuctionTrading/Presentation/AuctionTrading.WebHost/Program.cs
@@ -12,7 +12,7 @@ namespace AuctionTrading.WebHost
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddNpgsql<ApplicationDbContext>("Host=localhost;Port=5432;Database=AuctionTradingdb;UserName=postgres;Password=12345", options =>
+            builder.Services.AddNpgsql<ApplicationDbContext>("Host=localhost;Port=5432;UserName=postgres;Password=12345", options =>
             {
                 options.MigrationsAssembly("AuctionTrading.Infrastructure.EntityFramework");
 
